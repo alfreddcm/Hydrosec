230a23b7ccb4e208e9da2b80cc6a4fcc3acb7f80
alfreddcm <alfredmarcelinoii45@gmail.com>
2024-07-06 14:56:59 +0800

    1

--- resources/views/Owner/dashboard.blade.php
+++ resources/views/Owner/dashboard.blade.php
@@ -8,8 +8,8 @@
             <!-- Sidebar -->
      
             <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                  <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+                <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                  <img src="" alt="logo">
                   <span class="fs-4">Sidebar</span>
                 </a>
                 <hr>
