440fe4659e4c43d1f1bd47ab532d6126537b7276
alfreddcm <alfredmarcelinoii45@gmail.com>
2024-07-08 22:00:02 +0800

    HTML Draft 1

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/Owner/dashboard', function () {
     return view('Owner.dashboard');
 });
