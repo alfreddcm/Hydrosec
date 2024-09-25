5160e9d8c0ec1177f03444451f9f3d999473e257
Alfred Marcelino <alfred.c.marcelino@isu.edu.ph>
2024-09-25 14:23:46 +0800

    data

--- app/Events/SensorDataUpdated.php
+++ app/Events/SensorDataUpdated.php
@@ -5,6 +5,8 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
 
 class SensorDataUpdated implements ShouldBroadcast
 {
